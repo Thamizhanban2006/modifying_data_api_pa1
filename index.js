ad5258d2b7b6279c1162af4a7b4dedfddadc9a29
ThamizhAnban <thamizhanban2006@gmail.com>
2025-02-15 10:43:53 +0530

    completed

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const { resolve } = require('path');
+const schema = require('./schema');
 
 const app = express();
 const port = 3010;
@@ -9,6 +10,11 @@ app.use(express.static('static'));
 app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
+
+
+app.post('/menu',(req,res)=>{
+  
+})
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
